278c5aada2e95ffb53a0c21e118050667350c944
Fabian_Amaya <ozkaritogiofabis@gmail.com>
2024-12-02 15:53:27 -0500

    ya sirve el login
    agregue validación para el login estilos agregue las alertas a las que me corresponde

--- js/fijos.js
+++ js/fijos.js
@@ -1,9 +1,11 @@
-import { initHeader } from '../js/header.js';
-import { initFooter } from '../js/footer.js';
+import { initHeader } from './fijos/header.js';
+import { initFooter } from './fijos/footer.js';
 import { htmlLogin } from '../js/login/gral-html-login.js';
 import { initLogin } from '../js/login/login.js';
+import { validacionLogin } from '../js/login/validacion.js';
 
 initHeader();
 htmlLogin();
 initFooter();
 initLogin();
+validacionLogin();
---
+++ js/login/validacion.js
@@ -0,0 +1,36 @@
+export function validacionLogin() {
+  document.querySelector('.btn-ingresar').addEventListener('click', function(event){
+  event.preventDefault();
+  
+  const email = document.getElementById('email').value.trim();
+  const password = document.getElementById('password').value.trim();
+  
+  if (email ==='' || password === ''){
+      Swal.fire({
+          icon: "warning",
+          title: 'Campos Vacios', 
+          text: ' Por favor, completa todos los campos antes de continuar',
+          confirmButtonText:'Aceptar',
+        });
+        return; 
+  }
+  if (email ==='admin@hgw.com' && password ==='12345'){
+      Swal.fire({
+          icon: "success",
+          title: 'Inicio exitoso', 
+          text: ' Bienvenido Administrador',
+          confirmButtonText:'Ingresar',
+        }).then (()=>{
+          window.location.href= 'Admin/dashboard.html'
+  });
+        return; 
+  } else{
+      Swal.fire({
+          icon: "error",
+          title: 'Credenciales no validas', 
+          text: ' El correo o la contraseña son incorrectos',
+          confirmButtonText:'Reintentar',
+        });
+        }
+  });
+}
