ca896416420a293d639ff8f12813271cc9e616e5
matcar12345 <matcar1229@gmail.com>
2024-12-06 12:49:52 -0500

    modified:   Admin/Administrador/Index.html 	new file:   Admin/Administrador/javaScript/dashboard.js 	modified:   Admin/Administrador/javaScript/javaS.js 	new file:   Admin/Administrador/styles/Generador_dashboard.css 	modified:   Admin/Administrador/styles/header.css 	modified:   Admin/Administrador/styles/section.css 	modified:   Admin/Administrador/styles/usuario.css

--- Admin/Administrador/javaScript/javaS.js
+++ Admin/Administrador/javaScript/javaS.js
@@ -2,15 +2,18 @@ function nav_bar(){
     let bar = document.getElementById("nav-bar");
     let contenedor_btn_menu = document.getElementById("contenedor-btn-menu");
     let section = document.getElementById("section");
+    let generador_dashboard = document.getElementById("generador-dashboard");
     if(bar.className == "nav-bar"){
         bar.className = "nav-bar on";
         contenedor_btn_menu.className = "contenedor-btn-menu on";
         section.className = "on-section";
+        generador_dashboard.className = "generador-dashboard on-generador-dashboard";
     }
     else if(bar.className = "nav-bar on"){
         bar.className = "nav-bar";
         contenedor_btn_menu.className = "contenedor-btn-menu";
         section.className = "";
+        generador_dashboard.className = "generador-dashboard";
     }
 }
 function usuario(){
