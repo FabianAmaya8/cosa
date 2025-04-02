24705b5ddec866840a932fd6f20f92e9b76f0d72
Fabian_Amaya <123851947+FabianAmaya8@users.noreply.github.com>
2025-04-02 08:58:37 -0500

    botón chat bot y prueba de carrusel de productos

--- js/fijos.js
+++ js/fijos.js
@@ -1,11 +1,13 @@
 import { initHeader } from './fijos/header.js';
 import { initFooter } from './fijos/footer.js';
+import { ChatBot } from './fijos/chatbot.js';
 import { htmlLogin } from '../js/login/gral-html-login.js';
 import { initLogin } from '../js/login/login.js';
 import { validacionLogin } from '../js/login/validacion.js';
 
 initHeader();
 htmlLogin();
+ChatBot();
 initFooter();
 initLogin();
 validacionLogin();
